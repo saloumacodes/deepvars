f6d6827761e5e10bdefa536c7e58e44d005c7f33
saloumacodes <manelslim60@gmail.com>
2025-05-16 20:35:11 +0000

    Update prepare_deepvar_model.R

--- R/prepare_deepvar_model.R
+++ R/prepare_deepvar_model.R
@@ -1,84 +1,75 @@
-# Combined model: ----
-#' Deep VAR model setup
+#' Deep VAR model setup (custom NLL loss)
 #'
-#' @param deepvar_data
-#' @param num_units
-#' @param num_layers
-#' @param p_drop_out
-#' @param epsilon
-#' @param optimizer
+#' @param deepvar_data Processed data list from deepvars::prepare_deepvar_data
+#' @param num_units   Number of LSTM units per layer
+#' @param num_layers  Number of LSTM layers per series
+#' @param p_drop_out  Dropout probability between layers
+#' @param epsilon     Learning rate for optimizer
 #'
-#' @return
+#' @return A "deepvar_model" object containing compiled Keras models
 #' @export
-#'
-#' @importFrom keras `%>%`
-#'
-#' @author Patrick Altmeyer
 prepare_deepvar_model <- function(
     deepvar_data,
-    num_units = 50,
-    num_layers = 2,
-    p_drop_out = 0.5,
-    epsilon = 0.001,
-    optimizer = keras::optimizer_adam(learning_rate = epsilon)
+    num_units   = 50,
+    num_layers  = 2,
+    p_drop_out  = 0.5,
+    epsilon     = 0.001
 ) {
-
-  K <- deepvar_data$K
-  N <- deepvar_data$N
+  # Number of series and input dimensions
+  K         <- deepvar_data$K
   dim_input <- dim(deepvar_data$X)[2:3]
-
-  model_list <- lapply(
-    1:K,
-    function(k) {
-      # Build model:
-      list_of_layers <- lapply(
-        1:num_layers,
-        function(layer) {
-          list(
-            keras::layer_lstm(
-              units = num_units,
-              return_sequences = ifelse(layer < num_layers, TRUE, FALSE),
-              input_shape = dim_input
-            ),
-            keras::layer_dropout(rate = p_drop_out)
-          )
-        }
-      )
-
-      list_of_layers <- do.call(c, list_of_layers)
-
-      model <- keras::keras_model_sequential(list_of_layers) %>%
-        keras::layer_dense(units = 2, activation = "linear") %>%
-        tfprobability::layer_distribution_lambda(
-          function(x) {
-            tfprobability::tfd_normal(
-              loc = x[, 1, drop = FALSE],  # mean
-              scale = 1e-3 + tensorflow::tf$math$softplus(x[, 2, drop = FALSE])  # std deviation
-            )
-          }
-        )
-
-      # Negative log-likelihood loss
-      negloglik <- function(y_true, y_pred) {
-        -tensorflow::tf$reduce_mean(y_pred$log_prob(y_true))
-      }
-
-      # Compile model:
-      model %>%
-        keras::compile(
-          loss = negloglik,
-          optimizer = optimizer
-        )
-
-      return(model)
+  
+  # Build one model per series
+  model_list <- lapply(seq_len(K), function(k) {
+    # Input layer
+    input_layer <- keras::layer_input(shape = dim_input)
+    x <- input_layer
+    
+    # Add LSTM + Dropout layers
+    for (i in seq_len(num_layers)) {
+      return_seq <- i < num_layers
+      x <- keras::layer_lstm(
+        units            = num_units,
+        return_sequences = return_seq
+      )(x)
+      x <- keras::layer_dropout(rate = p_drop_out)(x)
     }
-  )
-
+    
+    # Final dense layer outputs 2 units: [mean, raw_scale]
+    preds <- keras::layer_dense(
+      units      = 2,
+      activation = "linear"
+    )(x)
+    
+    # Build the model
+    model <- keras::keras_model(
+      inputs  = input_layer,
+      outputs = preds
+    )
+    
+    # Custom Negative Log-Likelihood loss
+    nll_loss <- function(y_true, y_pred) {
+      mu        <- y_pred[, 1, drop = FALSE]
+      sigma_raw <- y_pred[, 2, drop = FALSE]
+      sigma     <- 1e-3 + tensorflow::tf$math$softplus(sigma_raw)
+      dist      <- tfprobability::tfd_normal(loc = mu, scale = sigma)
+      -tensorflow::tf$reduce_mean(dist$log_prob(y_true))
+    }
+    
+    # ✅ Compile model (Keras 3 compatible)
+    model$compile(
+      optimizer = keras::optimizer_adam(learning_rate = epsilon),
+      loss      = nll_loss
+    )
+    
+    return(model)
+  })
+  
+  # Package result
   deepvar_model <- list(
     model_list = model_list,
     model_data = deepvar_data
   )
-
-  class(deepvar_model) <- "deepvar_model"
+  class(deepvar_model) <- c("deepvar_model", "dvars_model")
   return(deepvar_model)
 }
